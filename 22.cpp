e46b5e782106fe6e9afba035177a377e07fc6809
Anatolii Sakhnik <sakhnik@gmail.com>
2016-12-22 23:11:16 +0200

    Solve day 22

--- 22.cpp
+++ 22.cpp
@@ -3,16 +3,20 @@
 #include <regex>
 #include <vector>
 #include <cassert>
+#include <queue>
 
 using namespace std;
 
-int Solve(istream &&is)
+void Solve(istream &&is)
 {
 	struct Data
 	{
 		int x, y;
 		int avail, used;
+
+		int distance = -1;
 	};
+
 
 	vector<Data> lines;
 	string line;
@@ -29,6 +33,7 @@ int Solve(istream &&is)
 		d.y = stoi(m[2]);
 		d.used = stoi(m[3]);
 		d.avail = stoi(m[4]);
+
 		lines.push_back(d);
 	}
 
@@ -39,11 +44,85 @@ int Solve(istream &&is)
 			if (i != j && lines[i].used && lines[i].used <= lines[j].avail)
 				++count;
 
-	return count;
+	cout << count << endl;
+
+	///////////////////////////////// Part 2 //////////////////////
+
+	int width{}, height{};
+	int _x{-1}, _y{-1}, capacity{};
+
+	for (auto d : lines)
+	{
+		if (width < d.x)
+			width = d.x;
+		if (height < d.y)
+			height = d.y;
+
+		if (d.used == 0)
+		{
+			_x = d.x;
+			_y = d.y;
+			capacity = d.avail;
+		}
+	}
+
+	++width;
+	++height;
+	int Gx{width - 1}, Gy{0};
+
+	// Alignment should be the same as input lines
+	auto idx = [height](int x, int y)
+	{
+		return x * height + y;
+	};
+
+	queue<pair<int,int>> to_search;
+	to_search.emplace(_x, _y);
+	lines[idx(_x, _y)].distance = 0;
+
+	while (!to_search.empty())
+	{
+		auto xy = to_search.front();
+		to_search.pop();
+
+		if (xy.first == Gx && xy.second == Gy)
+			break;
+
+		auto try_go = [&](int dx, int dy)
+		{
+			auto x = xy.first + dx;
+			auto y = xy.second + dy;
+			if (x < 0 || x >= width)
+				return;
+			if (y < 0 || y >= height)
+				return;
+			auto &src = lines[idx(xy.first, xy.second)];
+			assert(src.x == xy.first && src.y == xy.second);
+			auto &dst = lines[idx(x, y)];
+			assert(dst.x == x && dst.y == y);
+
+			if (dst.used <= (src.avail+src.used) && dst.distance == -1)
+			{
+				dst.distance = src.distance + 1;
+				to_search.emplace(x, y);
+			}
+		};
+
+
+		try_go(-1, 0);
+		try_go(1, 0);
+		try_go(0, -1);
+		try_go(0, 1);
+	}
+
+	auto distance = lines[idx(Gx, Gy)].distance;
+	assert(distance != -1);
+	distance += 5 * (width - 2);
+	cout << distance << endl;
 }
 
 int main()
 {
-	cout << Solve(ifstream("22.txt")) << endl;
+	Solve(ifstream("22.txt"));
 	return 0;
 }
